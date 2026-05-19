0e80d346dc14e84e4510cef77f22a48ced9042d6
TjayAmit <tristanjayamit0813@gmail.com>
2026-05-19 23:45:21 +0800

    refactor: SemesterSeeder now only has current semester

--- database/seeders/SemesterSeeder.php
+++ database/seeders/SemesterSeeder.php
@@ -11,30 +11,6 @@ class SemesterSeeder extends Seeder
     {
         $semesters = [
             [
-                'name' => 'First Semester AY 2024-2025',
-                'academic_year' => '2024-2025',
-                'semester_type' => 'first',
-                'start_date' => '2024-08-12',
-                'end_date' => '2024-12-20',
-                'is_current' => false,
-            ],
-            [
-                'name' => 'Second Semester AY 2024-2025',
-                'academic_year' => '2024-2025',
-                'semester_type' => 'second',
-                'start_date' => '2025-01-06',
-                'end_date' => '2025-05-23',
-                'is_current' => false,
-            ],
-            [
-                'name' => 'First Semester AY 2025-2026',
-                'academic_year' => '2025-2026',
-                'semester_type' => 'first',
-                'start_date' => '2025-08-11',
-                'end_date' => '2025-12-19',
-                'is_current' => false,
-            ],
-            [
                 'name' => 'Second Semester AY 2025-2026',
                 'academic_year' => '2025-2026',
                 'semester_type' => 'second',
